63ad6fc7dbe56d0bfaeb5c4b284912255f137560
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2024-04-04 14:18:25 +0500

    aaa

--- src/components/FeaturedProduct/FeaturedProduct.jsx
+++ src/components/FeaturedProduct/FeaturedProduct.jsx
@@ -11,6 +11,25 @@ const FeaturedProduct = () => {
           <div className="link">Enjoy the Outdoors</div>
           <div className="link">Gaming Essentials</div>
         </div>
+        <section className="par7">
+          <div className="par2">
+            <div className="part1">
+              <img src="./images/rrr.jpg" alt="" />
+            </div>
+            <div className="part5">
+              <div className="all3">
+                <h1>
+                  Redmi <span>13</span> pro+{" "}
+                </h1>
+                <h3 className="p1">Xiaomi Fan Festival Special Edition</h3>
+                <div className="p2">
+                  Limited availability | Mystic Silver Learn more
+                </div>
+                <button>Learn more </button>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
